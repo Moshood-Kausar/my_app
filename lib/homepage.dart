f3540374feb2d005d91379043336d13e96e28c85
Kausar Moshood <kausarmoshood@gmail.com>
2022-08-17 17:34:41 +0100

    first commit

---
+++ lib/homepage.dart
@@ -0,0 +1,93 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellow,
+      appBar: AppBar(
+          title: const Text('OUI BOOTCAMP'),
+          elevation: 0,
+          leading: const Icon(Icons.person)),
+      endDrawer: Drawer(
+        //backgroundColor: Colors.yellow,
+        child: ListView(
+          children: const [
+            DrawerHeader(
+              decoration: BoxDecoration(color: Colors.black),
+              child: Text('Student Profile',
+                  style: TextStyle(color: Colors.white)),
+            ),
+            ListTile(
+              title: Text(
+                'Kausar',
+                style: TextStyle(fontSize: 30),
+              ),
+              subtitle: Text('my name'),
+              isThreeLine: true,
+              leading: Icon(Icons.person),
+              trailing: Icon(Icons.phone),
+              tileColor: Colors.grey,
+            ),
+            ListTile(
+              title: Text(
+                'Gender',
+                style: TextStyle(color: Colors.yellow),
+              ),
+              tileColor: Colors.blue,
+              subtitle: Text('Female', style: TextStyle(fontSize: 30)),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.phone),
+        backgroundColor: Colors.orange,
+        foregroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Container(
+          height: 200,
+          width: 200,
+          color: Colors.pink,
+          child: Center(
+            child: Text(
+              'Flutter Bootcamp',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(currentIndex: 0, items: const [
+        BottomNavigationBarItem(
+          backgroundColor: Colors.purple,
+          icon: Icon(Icons.home),
+          label: 'HomePage'
+        ),
+         BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          label: 'Profile'
+        ),
+         BottomNavigationBarItem(
+          icon: Icon(Icons.phone),
+          label: 'Contact'
+        ),
+         BottomNavigationBarItem(
+          icon: Icon(Icons.phone),
+          label: 'Contact'
+        ),
+      ],
+      ),
+    );
+  }
+}
